90e2283cd1845001a394da15865148405d7fa0bf
ralevn <niki_ralev@hotmail.com>
2018-03-20 17:13:49 +0200

    Second commit

--- processes.sh
+++ processes.sh
@@ -5,8 +5,8 @@
 
 ps -aux|awk '$3 != 0.0 && $4 != 0.0 && NR !=1 {print $1," ",$11}'|while read usr cmd  
 do  if [ $usr == "nikira" ]; then  # !!!! IMPORTANT there is space between brackets "[" and content inside
-	 printf "%-27s %-40s\n" "You are performing" $cmd 
-    else printf "%-27s %-40s\n" "User $usr is performing" $cmd  
+	 printf "%-28s %-40s\n" "You are performing" $cmd 
+    else printf "%-28s %-40s\n" "User $usr is performing" $cmd  
     fi
 done
 
