f523ed84151ac68fa217613cb47c714fc5a2f23e
ralevn <niki_ralev@hotmail.com>
2018-03-24 12:47:53 +0200

    list of lists and tranpose rows to cols + task :)

--- zipped.py
+++ zipped.py
@@ -1,3 +1,31 @@
+"""
+Input Format
+
+The first line contains  and  separated by a space. 
+ The next  lines contains the space separated marks obtained by students in a particular subject. 
+
+Output Format
+
+Print the averages of all students on separate lines.
+The averages must be correct up to  decimal place.
+
+Sample Input
+
+5 3
+89 90 78 93 80
+90 91 85 88 86  
+91 92 83 89 90.5
+
+Sample Output
+
+90.0 
+91.0 
+82.0 
+90.0 
+85.5   
+
+"""
+
 studNum,subjNum = map(int,raw_input().split())
 subjLst =[]
 
