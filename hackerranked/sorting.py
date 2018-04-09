5b6ba3e1267b3a96d51d09e811b89e79a5c32757
ralevn <niki_ralev@hotmail.com>
2018-04-09 18:06:51 +0300

    improved sorting with swaping (no tmp var)

--- hackerranked/sorting.py
+++ hackerranked/sorting.py
@@ -9,11 +9,9 @@ import sys
 def insertionSort2(n, arr):
     # Complete this function
     for i in range(1, len(arr)):
-        temp = arr[i]
         while i > 0 and temp < arr[i-1]:
-            arr[i] = arr[i-1]
+            arr[i],arr[i-1] = arr[i-1],arr[i]
             i -= 1
-        arr[i] = temp
         print (' '.join(str(i) for i in arr))
         
 if __name__ == "__main__":
